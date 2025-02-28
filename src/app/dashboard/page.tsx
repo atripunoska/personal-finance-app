b7f0b3bc41899e6f844a8b23b8203d7d7e850cb4
atripunoska <atripunoska@gmail.com>
2025-02-28 14:11:10 +0100

    feat: add data from db

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,61 @@
+import { fetchBalance, fetchTransactions } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-export default function Dashboard() {
-  return <div>Overview</div>;
+export default async function Dashboard() {
+  const balance = await fetchBalance();
+  const transactions = await fetchTransactions();
+  return (
+    <main>
+      <h1 className="text-xl font-bold">Overview</h1>
+      <div className="flex gap-2">
+        {balance.map((item) => {
+          return (
+            <div
+              className="rounded p-4 justify-content flex flex-col bg-gray-900 text-white"
+              key={Math.random()}
+            >
+              <p>{item.expenses}$</p>
+              <div className="text-2xl">dsadsa</div>
+            </div>
+          );
+        })}
+      </div>
+
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th colSpan={2}>Transactions</th>
+
+            <th>
+              <Link href="/dashboard/transactions">View all</Link>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((item, index) => {
+            if (index > 4) return;
+            return (
+              <tr key={Math.random()}>
+                <td>
+                  <Image
+                    src={"/" + item.avatar}
+                    alt={item.name}
+                    className="rounded size-[50px] rounded-full"
+                    width={50}
+                    height={50}
+                  />
+                </td>
+                <td> {item.name}</td>
+                <td>
+                  <div>{item.amount}</div> <div>{item.date}</div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
 }
--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -3,39 +3,35 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-
-const links = [
-  { name: "Overview", href: "/dashboard" },
-  {
-    name: "Transactions",
-    href: "/dashboard/transactions",
-  },
-  {
-    name: "Budgets",
-    href: "/dashboard/budgets",
-  },
-
-  { name: "Pots", href: "/dashboard/pots" },
-  {
-    name: "Recurring Bills",
-    href: "/dashboard/recurring-bills",
-  },
-];
+import { fetchPages } from "@/lib/data";
+import { useState, useEffect } from "react";
 
 export default function NavLinks() {
   const pathname = usePathname();
+
+  const [pages, setPages] = useState<{ name: string; path: string }[]>([]);
+
+  useEffect(() => {
+    async function fetchSidebarPages() {
+      const res = await fetchPages();
+      setPages(res);
+    }
+    fetchSidebarPages();
+  }, []);
+
+  console.log(pages, "pages from nav-links.tsx");
   return (
     <>
-      {links.map((link) => {
+      {pages?.map((link) => {
         return (
           <Link
-            key={link.name}
-            href={link.href}
+            key={link?.name}
+            href={link?.path}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 e rounded-md p-3 text-sm font-medium hover:bg-white hover:text-gray-900 md:flex-none md:justify-start md:p-2 md:px-3 ",
               {
-                "bg-white text-gray-900": pathname === link.href,
-                "text-white bg-gray-900": pathname !== link.href,
+                "bg-white text-gray-900": pathname === link.path,
+                "text-white bg-gray-900": pathname !== link.path,
               }
             )}
           >
