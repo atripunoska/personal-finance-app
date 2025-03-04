597c260404c4dd44803d5da2ca989ae746db9d8b
atripunoska <atripunoska@gmail.com>
2025-03-04 14:47:48 +0100

    feat: add basic pots page

--- src/app/dashboard/pots/page.tsx
+++ src/app/dashboard/pots/page.tsx
@@ -1,5 +1,30 @@
+import PotCard from "@/app/ui/pots/card";
+import { Button } from "@/components/ui/button";
+import { fetchPots } from "@/lib/data";
 import React from "react";
 
-export default function Pots() {
-  return <div>Pots</div>;
+export default async function Pots() {
+  const pots = await fetchPots();
+  return (
+    <>
+      <div className="flex justify-between">
+        <h3 className="font-bold font-public-sans text-3xl mb-3">Pots</h3>
+        <Button className="font-bold font-public-sans">+ Add New Pot</Button>
+      </div>
+
+      <div className="grid grid-cols-2 gap-3">
+        {pots.map((item) => {
+          return (
+            <PotCard
+              category={item.theme}
+              title={item.name}
+              key={item.name}
+              target={item.target}
+              total={item.total}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
---
+++ src/app/ui/pots/card.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import React, { useState } from "react";
+import { Modal } from "../modal";
+
+export default function PotCard({
+  category,
+  title,
+  target,
+  total,
+}: {
+  category: string;
+  title: string;
+  target: number;
+  total: number;
+}) {
+  const progress = (total / target) * 100;
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <Card className="p-4">
+      <div className="flex gap-3 items-center">
+        <div
+          className="w-4 h-4 rounded-full flex"
+          style={{ backgroundColor: `${category}` }}
+        ></div>
+        <h3 className="font-bold text-xl text-grey-900 font-public-sans">
+          {title}
+        </h3>
+      </div>
+      <div className="flex justify-between">
+        <div className="text-grey-300 font-bold font-public-sans">
+          Total Saved
+        </div>
+        <div className="text-grey-900 font-bold text-3xl font-public-sans">
+          $ {total}
+        </div>
+      </div>
+      <Progress value={progress} indicatorColor={category} />
+      <div className="flex justify-between">
+        <div className="text-grey-300 font-bold font-public-sans">
+          {Math.round(progress * 100) / 100}%
+        </div>
+        <div className="text-grey-300 font-semibold font-public-sans">
+          Total savings of ${total}
+        </div>
+      </div>
+      <div className="flex gap-2 justify-between">
+        <Button
+          className="font-bold font-public-sans text-grey-900 bg-grey-100 flex flex-grow border-2 border-grey-100 hover:bg-white hover:text-black hover:border-2"
+          onClick={() => setModalOpen(true)}
+        >
+          + Add Money
+        </Button>
+        <Button className="font-bold font-public-sans text-grey-900 bg-grey-100 border-2 border-grey-100 flex flex-grow hover:bg-white hover:text-black hover:border-2">
+          Withdraw
+        </Button>
+      </div>
+
+      <Modal
+        open={modalOpen}
+        titleContent={<h1> Close </h1>}
+        secondaryFn={() => setModalOpen(false)}
+        content={
+          <>
+            <h2>This is a modal</h2>
+            <p>
+              You can close it by pressing Escape key, pressing close, or
+              clicking outside the modal.
+            </p>
+          </>
+        }
+      />
+    </Card>
+  );
+}
