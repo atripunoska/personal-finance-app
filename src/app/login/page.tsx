9dce82548925ae5add0ed64fef16a4f0a98f0252
atripunoska <atripunoska@gmail.com>
2025-03-05 11:24:14 +0100

    feat: add auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
+import LoginForm from "../ui/login-form";
+import Image from "next/image";
 
 export default function Login() {
   return (
     <div className="flex flex-row items-center justify-center h-screen bg-amber-50 gap-2">
-      <div className="w-20">
-        <img src="/images/Sidebar.png" alt="Sidebar" className="h-full" />
+      <div className="w-1/3 gap-3">
+        <Image
+          src="/./assets/images/Sidebar.png"
+          alt="Sidebar"
+          width={1400}
+          height={200}
+        />
       </div>
-      <div className="w-80">
-        <div className="bg-white w-auto p-3">
-          <input type="text" className="border" />
-        </div>
+      <div className="w-2/3">
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
