0931e7c5ac60586e8f256d618c95fb2e2c8a29c7
atripunoska <atripunoska@gmail.com>
2025-03-10 13:51:24 +0100

    feat: add TotalBillsCard component

---
+++ src/app/ui/recurring-bills/TotalBillsCard.tsx
@@ -0,0 +1,25 @@
+import { USDollar } from "@/lib/utils";
+import React from "react";
+import Image from "next/image";
+
+export default function TotalBillsCard({
+  totalAmount,
+}: {
+  totalAmount: number;
+}) {
+  return (
+    <div className="bg-grey-900 text-white p-6 rounded-md flex flex-col ">
+      <Image
+        src="/./assets/images/icon-recurring-bills.svg"
+        width={30}
+        height={30}
+        alt="Recurring Bills icon"
+        className="mb-6"
+      />
+      <div className="font-light mb-2">Total Bills</div>
+      <span className="font-bold text-3xl">
+        {USDollar.format(Math.abs(totalAmount))}
+      </span>
+    </div>
+  );
+}
