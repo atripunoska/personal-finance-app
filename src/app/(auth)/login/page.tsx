77f91a79ab8cd7a00d87161983d2569884853cec
atripunoska <atripunoska@gmail.com>
2025-03-18 09:23:31 +0100

    refactor: update style on login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,17 +5,17 @@ import LoginForm from "@/app/ui/login-form";
 
 export default function Login() {
   return (
-    <div className="flex flex-row items-center justify-center h-screen bg-amber-50 md:gap-2">
-      <div className="md:w-1/3 gap-3">
+    <div className="flex flex-row items-center justify-center h-screen bg-amber-50 lg:gap-2">
+      <div className="lg:w-1/3 gap-3">
         <Image
-          src="/./assets/images/Sidebar.png"
+          src="/assets/images/Sidebar.png"
           alt="Sidebar"
           width={1400}
           height={200}
-          className="hidden md:flex"
+          className="hidden lg:flex"
         />
       </div>
-      <div className="md:w-2/3">
+      <div className="lg:w-2/3">
         <Suspense>
           <LoginForm />
         </Suspense>
