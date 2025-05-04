8470f57275f38cbcfc99ae113a71281cb4b56c2d
atripunoska <atripunoska@gmail.com>
2025-05-04 18:53:13 +0200

    refactor: remove comment

--- src/app/ui/budgets/BudgetCard.tsx
+++ src/app/ui/budgets/BudgetCard.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { Card, CardContent } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
