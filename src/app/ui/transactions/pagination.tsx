dc17ad0a2bb66652fd0de5d90efe14f154aeb0f6
atripunoska <atripunoska@gmail.com>
2025-05-16 16:33:19 +0200

    style: add border on hover on pagination

--- src/app/ui/transactions/pagination.tsx
+++ src/app/ui/transactions/pagination.tsx
@@ -74,7 +74,8 @@ function PaginationNumber({
       'rounded-l-md': position === 'first' || position === 'single',
       'rounded-r-md': position === 'last' || position === 'single',
       'z-10 bg-grey-900 border-grey-900 text-white': isActive,
-      'hover:bg-gray-800 hover:text-white': !isActive && position !== 'middle',
+      'hover:bg-gray-800 hover:border hover:border-gray-800 hover:text-white':
+        !isActive && position !== 'middle',
       'text-gray-300': position === 'middle',
     }
   );
@@ -101,7 +102,7 @@ function PaginationArrow({
     'flex h-10 w-10 items-center justify-center rounded-md border',
     {
       'pointer-events-none text-gray-300': isDisabled,
-      'hover:bg-gray-100': !isDisabled,
+      'hover:bg-gray-100 hover:border hover:border-gray-100': !isDisabled,
       'mr-2 md:mr-4': direction === 'left',
       'ml-2 md:ml-4': direction === 'right',
     }
