89830a7a1fb0dbcf0fd65ae5c8c3953c18d8b954
atripunoska <atripunoska@gmail.com>
2025-05-21 19:20:44 +0200

    feat: add new dependencies for jest

--- jest.config.ts
+++ jest.config.ts
@@ -96,7 +96,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -143,8 +145,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
-
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -182,7 +183,6 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
@@ -194,7 +194,7 @@ const config: Config = {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  // verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,27 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+import fetch, { Request, Response, Headers } from 'node-fetch';
+
+if (typeof global.TextEncoder === 'undefined') {
+  // @ts-ignore
+  global.TextEncoder = TextEncoder;
+}
+if (typeof global.TextDecoder === 'undefined') {
+  // @ts-ignore
+  global.TextDecoder = TextDecoder;
+}
+
+// Polyfill fetch, Request, Response, Headers for Node.js
+if (typeof global.fetch === 'undefined') {
+  global.fetch = fetch;
+}
+if (typeof global.Request === 'undefined') {
+  global.Request = Request;
+}
+if (typeof global.Response === 'undefined') {
+  global.Response = Response;
+}
+if (typeof global.Headers === 'undefined') {
+  global.Headers = Headers;
+}
