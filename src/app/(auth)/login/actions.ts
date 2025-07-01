5027fe76a8adc41c8ceae3290699f61c0b47ef0c
atripunoska <atripunoska@gmail.com>
2025-07-01 10:04:17 +0200

    refactor: update const

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -13,7 +13,8 @@ export async function login(formData: FormData) {
     password: formData.get('password') as string,
   };
 
-  const { data, error } = await supabase.auth.signInWithPassword(data);
+  const { data: authData, error } =
+    await supabase.auth.signInWithPassword(data);
   if (error) {
     throw new Error(error.message);
   }
